c1db96868ea4f58d73b57870921fce7533b47d83
= <=>
2022-12-12 10:14:04 +0100

    updated requirements file ad allowed host in settings file

--- church_website/settings.py
+++ church_website/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'jpcm.ushmoney.net']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
