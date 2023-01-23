7085dc4ac2d1f83ef0991cb479f673205c24f954
winninggodspower <winninggodspower@gmail.com>
2023-01-23 15:58:39 +0100

    using secret key from .env file

--- church_website/settings.py
+++ church_website/settings.py
@@ -23,7 +23,7 @@ load_dotenv(os.path.join(project_folder, '.env'))
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a8il450%mg$&ubsf5ba+c&%!+^0ut1955n55(s0nbxn^xllp7g'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
